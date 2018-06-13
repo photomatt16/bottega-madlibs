9e2eb2982be73e2131064344b34899cf81f1cc2a
Matthew Warner <mattwarner14@yahoo.com>
2018-06-13 10:12:48 -0600

    built our first class component for the imputs container, and built an imput functional component to render custom imputs

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Bottega Madlibs</h1>
+       <div className="home">
       </div>
     );
   }
